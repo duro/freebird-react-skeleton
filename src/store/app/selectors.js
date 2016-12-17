82b712211609a71edb6dc486faf65fbf27aca416
Adam Duro <adam@duromedia.com>
2016-12-16 23:25:43 -0800

    Get rid of unused selectors file

--- src/store/app/selectors.js
+++
@@ -1,6 +0,0 @@
-/**
- * Get the window size
- * @param  {object} state The global redux state
- * @return {ImmutableMap} A map with width/height of window
- */
-export const windowSizeSelector = (state) => state.app.get('window')
